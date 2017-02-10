3aded2045ee364c84806c122d5aa0ffd370bae6a
Justin Azoff <jazoff@illinois.edu>
2017-02-10 15:12:28 -0500

    depend on updated ninfo

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='ninfo-plugin-httptitle',
     packages = find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=[
-        "ninfo>=0.6.0",
+        "ninfo>=0.6.1",
         "requests>=2.0.0",
         "BeautifulSoup>=3.0.0",
     ],
